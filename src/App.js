afcff94f566a159a86c66ec5c7a5622056e375b1
Giulia <giulialake@gmail.com>
2017-12-12 23:27:09 -0500

    made board into functional component

--- src/App.js
+++ src/App.js
@@ -5,29 +5,79 @@ import './App.css';
 import Lightbox from './Lightbox';
 
 class App extends Component {
-  state = {
-    shown:false
+
+  constructor(props) {
+    super(props);
+
+    this.changeRow = this.changeRow.bind(this)
+    this.renderEnd = this.renderEnd.bind(this)
+    this.changeColor = this.changeColor.bind(this)
+
+    let colors = ["#40e0d0",
+      "#654321",
+      "#800080",
+      "#ffa500",
+      "#0000ff",
+      "#ff0000",
+      "#008000",
+      "#ffff00"];
+    let answer = [];
+    for (var i = 0; i < 4; i++) {
+      var rand = Math.floor(Math.random() * colors.length);
+      answer.push(colors[rand]);
+    }
+
+    this.state = {
+      currentColor: "#371c0e",
+      currentRow: 1,
+      answer: answer,
+      colors: colors,
+      guessCount: 0,
+      shown: false,
+      won: null
+
+    }
   }
-  showLightbox =()=>{
-    this.setState({shown:true})
-    document.getElementsByTagName('body')[0].className+='overflow';
+
+  changeColor(color) {
+    this.setState({ currentColor: color })
   }
-  hideLightbox =()=>{
-    this.setState({shown:false})
-    document.getElementsByTagName('body')[0].className='';  
+
+  changeRow() {
+
+    this.setState({ currentRow: this.state.currentRow + 1, guessCount: this.state.guessCount + 1 })
+    console.log(this.state.guessCount)
+    if (this.state.guessCount === 9) {
+      this.renderEnd(false);
+    }
+  }
+
+  renderEnd(result) {
+    console.log(result)
+    this.setState({ won: result })
+    alert(result)
+  }
+
+  showLightbox = () => {
+    this.setState({ shown: true })
+    document.getElementsByTagName('body')[0].className += 'overflow';
   }
   
+  hideLightbox = () => {
+    this.setState({ shown: false })
+    document.getElementsByTagName('body')[0].className = '';
+  }
+
   render() {
     return (
       <div className="App">
-      <div className="heading"><h1>Mastermind Game</h1>
-        <p className="instructions" onClick={this.showLightbox}>How to play</p></div>
-        {this.state.shown&&
-          <Lightbox  hideLightbox={this.hideLightbox}/>
-          }
-         
+        <div className="heading"><h1>Mastermind Game</h1>
+          <p className="instructions" onClick={this.showLightbox}>How to play</p></div>
+        {this.state.shown &&
+          <Lightbox hideLightbox={this.hideLightbox} />
+        }
         <div className="trial"><div className="test bordertop"></div></div>
-        <Board />
+        <Board colors={this.state.colors} currentColor={this.state.currentColor} currentRow={this.state.currentRow} changeRow={this.changeRow} answer={this.state.answer} changeColor={this.changeColor} renderEnd={this.renderEnd} />
         <div className="trial"><div className="test borderbottom"></div></div>
       </div>
     );
--- src/Board.js
+++ src/Board.js
@@ -1,80 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Row from './Row';
 import Endgame from './Endgame';
 
-class Board extends Component {
+export default function Board(props) {
 
-    constructor(props) {
-        super(props);
-
-        this.changeRow = this.changeRow.bind(this)
-        this.renderEnd = this.renderEnd.bind(this)
-
-        let colors = ["#40e0d0",
-            "#654321",
-            "#800080",
-            "#ffa500",
-            "#0000ff",
-            "#ff0000",
-            "#008000",
-            "#ffff00"];
-        let answer = [];
-        for (var i = 0; i < 4; i++) {
-            var rand = Math.floor(Math.random() * colors.length);
-            answer.push(colors[rand]);
+    let firstChoiceArray = []
+    for (var k = 0; k < 2; k++) {
+        firstChoiceArray.push(<div className="empty" key={"a" + k}></div>)
+        for (var j = 0; j < 4; j++) {
+            firstChoiceArray.push(
+                <div className="choiceBoard top" key={props.colors[j + k * 4]}>
+                    <div className="bc choice" style={{ background: `radial-gradient(circle at 50px 50px, #000,${props.colors[j + k * 4]})` }} onClick={props.changeColor.bind(this, props.colors[j + k * 4])}>
+                    </div>
+                </div>
+            )
         }
-
-        this.state = {
-            currentColor: "#371c0e",
-            currentRow: 1,
-            answer: answer,
-            colors: colors,
-            guessCount: 0,
-            won: null
-        }
+        firstChoiceArray.push(<div id={"mycell" + k} key={"b" + k} className="choiceBoard top"></div>)
+        firstChoiceArray.push(<div key={"c" + k} className="empty"></div>)
     }
 
-    changeColor(color) {
-        this.setState({ currentColor: color })
+    let boardArray = []
+    for (var i = 1; i <= 10; i++) {
+        boardArray.push(<Row row={i} key={i} renderEnd={props.renderEnd} currentrow={props.currentRow} answer={props.answer} currentcolor={props.currentColor} changeRow={props.changeRow} guessCount={props.guessCount} />)
     }
 
-    changeRow() {
-        
-        this.setState({ currentRow: this.state.currentRow + 1, guessCount: this.state.guessCount + 1  })
-        console.log(this.state.guessCount)
-        if(this.state.guessCount===9){
-            this.renderEnd(false);
-        }
-    }
+    return (
+        <div className="container">
+            <div className="grid"> {firstChoiceArray}</div>
+            {boardArray}
+        </div>
+    );
 
-    renderEnd(result){
-        console.log(result)
-        this.setState({won:result})
-    }
-
-    render() {
-
-        let firstChoiceArray = []
-        for (var k=0; k<2; k++){
-            firstChoiceArray.push(<div className="empty" key={"a"+k}></div>)
-            for (var j = 0; j < 4; j++) {
-                firstChoiceArray.push(<div className="choiceBoard top" key={this.state.colors[j+k*4]}> <div className="bc choice"  style={{ background: `radial-gradient(circle at 50px 50px, #000,${ this.state.colors[j+k*4]})` }} onClick={this.changeColor.bind(this, this.state.colors[j+k*4])}></div></div>)
-            }
-            firstChoiceArray.push(<div id={"mycell"+k} key={"b"+k} className="choiceBoard top"></div>)
-            firstChoiceArray.push(<div key={"c"+k} className="empty"></div>)
-        }
-
-        let boardArray = []
-        for (var i = 1; i <= 10; i++) {
-            boardArray.push(<Row row={i} key={i} renderEnd={this.renderEnd} currentrow={this.state.currentRow} answer={this.state.answer} currentcolor={this.state.currentColor} changeRow={this.changeRow} guessCount={this.state.guessCount}/>)
-        }
-
-        return (
-            <div className="container">
-                <div className="grid"> {firstChoiceArray}</div>
-                {boardArray}
-            </div>
-        );
-    }
 }
-export default Board
