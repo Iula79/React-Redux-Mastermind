e0e5f7cca6a55733acbfc0e58a31b396590921d9
Giulia <giulialake@gmail.com>
2018-02-15 17:19:37 -0500

    added switch statement

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,10 @@
 import {combineReducers} from 'redux';
 import ColorsReducer from './reducer_colors.js';
 import GameReducer from './reducer_game.js';
-// import CurrentColorReducer from './reducer_current_color.js'
-// import AnswerReducer from './reducer_answer.js';
-// import CurrentRowReducer from './reducer_current_row.js';
-// import GuessCountReducer from './reducer_guess_count.js';
-// import SubmitRowReducer from './reducer_submit_row'
 
 const rootReducer = combineReducers({
     colors:ColorsReducer,
-    game: GameReducer,
-    // currentColor:CurrentColorReducer,
-    // answer:AnswerReducer,
-    // currentRow:CurrentRowReducer,
-    // guessCount:GuessCountReducer,
-    // row:SubmitRowReducer
-
+    game: GameReducer
 })
 
 export default rootReducer;
--- src/reducers/reducer_game.js
+++ src/reducers/reducer_game.js
@@ -1,14 +1,27 @@
-export default function (state = null, action) {
+const defaultState = {
+    colorSelected : 'gray',
+    guessCount : 0,
+    row : 0,
+    answer : []
+}
+
+function updateObject(oldObject, newValues) {
+    // Encapsulate the idea of passing a new object as the first parameter
+    // to Object.assign to ensure we correctly copy data instead of mutating
+    return Object.assign({}, oldObject, newValues);
+}
+
+export default function (state = defaultState, action) {
     console.log(state)
     switch (action.type) {
         case 'COLOR_SELECTED':
-            return action.payload;
+            return updateObject(state, { colorSelected: action.payload});
         case 'GUESS_COUNT':
-            return action.payload;
+            return updateObject(state, { guessCount: action.payload});
         case 'SUBMIT_ROW':
-            return action.payload;
+            return updateObject(state, { row: action.payload.row});
         case 'CREATE_ANSWER':
-            return action.payload;
+            return updateObject(state, {answer: action.payload});
     }
 
     return state
